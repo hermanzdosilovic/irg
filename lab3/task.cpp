b1599bbb1287ea64880666966e81ff45dc933073
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-04-07 02:54:44 +0200

    Point relationship with polygons

--- lab3/task.cpp
+++ lab3/task.cpp
@@ -1,43 +1,74 @@
 #include <stdio.h>
 #include <GL/freeglut.h>
+#include <glm/vec3.hpp>
+#include <glm/geometric.hpp>
+#include <iostream>
 #include "bresenham.h"
 
 const int kMaxNumberOfVertices = 50;
 const int kMaxNumberOfPolygons = 50;
 const int kWidth = 500, kHeight = 500;
 
-struct Point {
-  int x, y;
-} g_vertices[kMaxNumberOfPolygons + 1][kMaxNumberOfVertices + 1];
+struct Polygon {
+  glm::vec3 vertices[kMaxNumberOfVertices + 1];
+  int number_of_vertices;
+} g_polygons[kMaxNumberOfPolygons + 1];
 
-int g_number_of_vertices;
 int g_number_of_polygons;
+glm::vec3 g_point;
 
-Point g_point;
+std::ostream &operator<< (std::ostream &out, const glm::vec3 &v) {
+  out << "(" << v.x << ", " << v.y << ")";
+  return out;
+}
 
-void drawPolygon(Point *vertices, int number_of_vertices) {
-  if (number_of_vertices == 0) {
-    return;
+int drawPolygon(Polygon polygon) {
+  if (polygon.number_of_vertices == 0) {
+    return -1;
   }
-
-  for (int i = 0; i < number_of_vertices - 1; i++) {
+  
+  glm::vec3 *vertices = polygon.vertices;
+  for (int i = 0; i < polygon.number_of_vertices - 1; i++) {
     drawLine(vertices[i].x, vertices[i].y, vertices[i + 1].x, vertices[i + 1].y);
   }
 
   drawLine(
-    vertices[number_of_vertices - 1].x,
-    vertices[number_of_vertices - 1].y,
+    vertices[polygon.number_of_vertices - 1].x,
+    vertices[polygon.number_of_vertices - 1].y,
     vertices[0].x,
     vertices[0].y
   );
+
+  return 0;
 }
 
 void addPoint(int x, int y) {
-  if (g_number_of_vertices + 1 > kMaxNumberOfVertices) {
+  int *i = &g_polygons[g_number_of_polygons].number_of_vertices;
+  if (*i + 1 > kMaxNumberOfVertices) {
     printf("Maximum number of vertices exceeded!\n");
     return;
   }
-  g_vertices[g_number_of_polygons][g_number_of_vertices++] = {x, y};
+
+  g_polygons[g_number_of_polygons].vertices[*i] = glm::vec3(x, y, 1);
+  *i += 1;
+}
+
+int polygonPointRelation(glm::vec3 point, Polygon polygon) {
+  int N = polygon.number_of_vertices;
+  glm::vec3 edges[polygon.number_of_vertices + 1];
+  for (int i = 0; i < N; i++) { // expecting vertices in clockwise direction
+    edges[i] = glm::cross(polygon.vertices[i], polygon.vertices[(i + 1)%N]);
+  }
+
+  int inside = 1;
+  for (int i = 0; i < N; i++) {
+    if (!(glm::dot(point, edges[i]) >= 0)) { // >= because of reverse coordinate system (y is upside down)
+      inside = 0;
+      break;
+    }
+  }
+  
+  return inside;
 }
 
 void mouse(int button, int state, int x, int y) {
@@ -49,11 +80,20 @@ void mouse(int button, int state, int x, int y) {
       return;
     }
     printf("Drawing polygon P%d.\n", g_number_of_polygons);
-    drawPolygon(g_vertices[g_number_of_polygons++], g_number_of_vertices);
-
-    g_number_of_vertices = 0;
+    int ret = drawPolygon(g_polygons[g_number_of_polygons]);
+    if (ret == 0) {
+      printf("  Success.\n");
+      g_number_of_polygons++;
+    } else {
+      printf("  Failed. Polygon has no vertices.\n");
+    }
   } else if (button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN) {
-    g_point = {x, y};
+    g_point = glm::vec3(x, y, 1);
+    std::cout << "Point " << g_point << " relation with polygons:" << std::endl;
+    for (int i = 0; i < g_number_of_polygons; i++) {
+      int is_inside = polygonPointRelation(g_point, g_polygons[i]);
+      std::cout << (is_inside ? "  Inside" : "  Outside") << " of polygon P" << i << std::endl;
+    }
   }
 }
 
