5edbfbb7022c9c64db65eebd7dea4dfc5e20f32e
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-06-07 16:07:05 +0200

    Lab7 some changes

--- lab7/task.cpp
+++ lab7/task.cpp
@@ -7,6 +7,7 @@
 #include <glm/mat4x4.hpp>
 #include <glm/matrix.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <float.h>
 
 int g_width = 600, g_height = 600;
 
@@ -16,6 +17,9 @@ struct Triangle {
 
 std::vector<glm::vec4> g_points, g_polygon;
 std::vector<Triangle> g_triangles;
+
+float g_min_x = FLT_MAX, g_min_y = FLT_MAX, g_min_z = FLT_MAX;
+float g_max_x = FLT_MIN, g_max_y = FLT_MIN, g_max_z = FLT_MIN;
 
 glm::vec4 g_camera = glm::vec4(20, 20, 20, 1),
           g_view = glm::vec4(0, 0, 0, 1),
@@ -41,7 +45,6 @@ std::ostream &operator<< (std::ostream &out, const glm::vec4 &v) {
   out << "(" << v.x << ", " << v.y << ", " << v.z << ", " << v.w << ")";
   return out;
 }
-
 glm::mat4 viewTransformMatrix(glm::vec4 camera, glm::vec4 view) {
   glm::mat4 T1 = glm::transpose(glm::mat4(
             1,         0,         0, 0,
@@ -120,35 +123,41 @@ glm::mat4 perspectiveTransformMatrix(glm::vec4 camera, glm::vec4 view) {
    ));
  }
 
-glm::vec4 transformPoint(glm::vec4 point, glm::mat4 matrix) {
-  glm::vec4 transformedPoint = point * matrix;
-  return transformedPoint / transformedPoint.w;
-}
-
 void display() {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-  gluLookAt(g_camera.x, g_camera.y, g_camera.z, g_view.x, g_view.y, g_view.z, g_up.x, g_up.y, g_up.z);
   glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glScalef(10, 10, 10);
- 
-  glm::mat4 S = scaleMatrix(10);
+  glClear(GL_COLOR_BUFFER_BIT/* | GL_DEPTH_BUFFER_BIT*/);
+
+  float size_x = g_max_x - g_min_x;
+  float size_y = g_max_y - g_min_y;
+  float size_z = g_max_z - g_min_z;
+  float center_x = (g_max_x + g_min_x)/2;
+  float center_y = (g_max_y + g_min_y)/2;
+  float center_z = (g_max_z + g_min_z)/2;
+  float scale = 2/fmaxf(size_x, fmaxf(size_y, size_z));
+
+  glPushMatrix();
+  glScalef(scale, scale, scale);
+  gluLookAt(g_camera.x, g_camera.y, g_camera.z, g_view.x, g_view.y, g_view.z, g_up.x, g_up.y, g_up.z);
+  glTranslatef(-center_x, -center_y, -center_z);
+  // glCullFace(GL_BACK);
+
+  glm::mat4 S = scaleMatrix(scale);
   glm::mat4 T = viewTransformMatrix(g_camera, g_view);
   glm::mat4 P = perspectiveTransformMatrix(g_camera, g_view);
-  glm::mat4 transformMatrix = S*T*P;
+  glm::mat4 M = S*T*P;
 
   glColor3f(0.0f, 0.0f, 0.0f);
   glLineWidth(1);
-  glBegin(GL_LINES);
   for (auto t : g_triangles) {
-    glm::vec4 v1 = transformPoint(t.v1, transformMatrix);
-    glm::vec4 v2 = transformPoint(t.v2, transformMatrix);
-    glm::vec4 v3 = transformPoint(t.v3, transformMatrix);
+    glBegin(GL_LINES);
+    glm::vec4 v1 = t.v1*M, v2 = t.v2*M, v3 = t.v3*M;
     glm::vec3 n = glm::cross(glm::vec3(v2 - v1), glm::vec3(v3 - v1));
-    glm::vec4 c = glm::vec4((v1.x + v2.x + v3.x)/3, (v1.y + v2.y + v3.y)/3, (v1.z + v2.z + v3.z)/3, 1);
+    glm::vec4 c = (v1 + v2 + v3) / glm::vec4(3, 3, 3, 3);
     glm::vec3 p = glm::vec3(g_camera - c);
-    if (glm::dot(n, p) < 0) {
+    double cosL = glm::dot(n, p)/glm::distance(n, p);
+    if (cosL < 1e-6) {
       continue;
     }
 
@@ -160,33 +169,36 @@ void display() {
 
     glVertex3f(t.v3.x, t.v3.y, t.v3.z);
     glVertex3f(t.v1.x, t.v1.y, t.v1.z);
+    glEnd();
   }
-  glEnd();
+  glPopMatrix();
 
-  glLineWidth(3);
+  gluLookAt(g_camera.x, g_camera.y, g_camera.z, g_view.x, g_view.y, g_view.z, g_up.x, g_up.y, g_up.z);
+  glLineWidth(2);
   glBegin(GL_LINES);
     glColor3f(1, 0, 0);
-    glVertex3f(0, 0, 0); glVertex3f(0.2, 0, 0);
+    glVertex3f(0, 0, 0); glVertex3f(1, 0, 0);
     glColor3f(0, 1, 0);
-    glVertex3f(0, 0, 0); glVertex3f(0, 0.2, 0);
+    glVertex3f(0, 0, 0); glVertex3f(0, 1, 0);
     glColor3f(0, 0, 1);
-    glVertex3f(0, 0, 0); glVertex3f(0, 0, 0.2);
+    glVertex3f(0, 0, 0); glVertex3f(0, 0, 1);
   glEnd();
 
   glFlush();
 }
 
-void updatePerspective() {
+void updatePerspective(int width, int height) {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-  glFrustum(-1, 1, -1, 1, glm::distance(g_camera, g_view)/10, 100*glm::distance(g_camera, g_view));
+  gluPerspective(45.0, (float)width/height, 1, 100);
+  // glFrustum(-1, 1, -1, 1, 1, 100);
 }
 
 void resize(int width, int height) {
   g_width = width;
   g_height = height;
   glViewport(0, 0, g_width, g_height);
-  updatePerspective();
+  updatePerspective(width, height);
 }
 
 int g_motion_start = 0, g_x = 0;
@@ -216,6 +228,7 @@ void motion(int x, int y) {
   float alpha = -(g_x - x) / glm::distance(g_camera, g_view);
   glm::mat4 R = glm::rotate(glm::mat4(), alpha, glm::vec3(g_up));
   g_camera = g_camera * R;
+  g_up = g_up * R;
   g_x = x;
 
   glutPostRedisplay();
@@ -229,6 +242,9 @@ int main(int argc, char **argv) {
       float x, y, z;
       sscanf(&line[2], "%f %f %f", &x, &y, &z);
       g_points.push_back(glm::vec4(x, y, z, 1));
+      g_min_x = fminf(g_min_x, x); g_max_x = fmaxf(g_max_x, x);
+      g_min_y = fminf(g_min_y, y); g_max_y = fmaxf(g_max_y, y);
+      g_min_z = fminf(g_min_z, z); g_max_z = fmaxf(g_max_z, z);
     } else if (line[0] == 'f') {
       int i, j, k;
       sscanf(&line[2], "%d %d %d", &i, &j, &k);
@@ -237,8 +253,11 @@ int main(int argc, char **argv) {
   }
   fclose(f);
 
+  std::cout << "MIN: " << g_min_x << " " << g_min_y << " " << g_min_z << std::endl;
+  std::cout << "MAX: " << g_max_x << " " << g_max_y << " " << g_max_z << std::endl;
+
   glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
   glutInitWindowSize(g_width, g_height);
   glutInitWindowPosition(700, 100);
   glutCreateWindow("Objects");
@@ -246,6 +265,9 @@ int main(int argc, char **argv) {
   glutReshapeFunc(resize);
   glutMouseFunc(mouse);
   glutMotionFunc(motion);
+  // glEnable(GL_CULL_FACE);
+  // glEnable(GL_DEPTH_TEST);
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
   glutMainLoop();
 
   return 0;
