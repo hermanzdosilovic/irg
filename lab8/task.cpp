be4bf804cfe51f1914a8fdcf1169934862ae981b
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-06-07 19:17:07 +0200

    Finished lab8

--- lab8/task.cpp
+++ lab8/task.cpp
@@ -3,18 +3,31 @@
 
 int g_width = 500, g_height = 500;
 
-double g_epsilon, g_u_min, g_u_max, g_v_min, g_v_max;
+double g_epsilon, g_complex_const_re, g_complex_const_im, g_u_min, g_u_max, g_v_min, g_v_max;
 int g_max_iterations;
 
+int g_fractal_mode = 0;
+
 int divergence_test(double re, double im, int limit) {
-  double _re = 0, _im = 0;
+  double _re, _im;
+  if (!g_fractal_mode) {
+    _re = 0;
+    _im = 0;
+  } else {
+    _re = re;
+    _im = im;
+    re = g_complex_const_re;
+    im = g_complex_const_im;
+  }
   for (int i = 1; i <= limit; i++) {
     double next_re = _re*_re - _im*_im + re;
     double next_im = 2*_re*_im + im;
     _re = next_re;
     _im = next_im;
     double modul2 = _re*_re + _im*_im;
-    if (modul2 > 4) return i;
+    if (modul2 > g_epsilon) {
+      return i;
+    }
   }
   return -1;
 }
@@ -41,7 +54,7 @@ void display() {
         if (n == -1) {
           glColor3f(0, 0, 0);
         } else {
-          glColor3f((double)n/g_max_iterations, 1-(double)n/g_max_iterations/2.0, 0.8-(double)n/g_max_iterations/3.0);
+          glColor3f((double)n/g_max_iterations/10.0,   (double)n/g_max_iterations, (double)n/g_max_iterations);
         }
         glVertex2i(x, y);
     }
@@ -51,12 +64,21 @@ void display() {
   glFlush();
 }
 
+void keyboard(unsigned char key, int x, int y) {
+  if (key == 'n' || key == 'N') {
+    g_fractal_mode++;
+    g_fractal_mode %= 2;
+    glutPostRedisplay();
+  }
+}
+
 int main(int argc, char **argv) {
   FILE *f = fopen(argv[1], "r");
   fscanf(f, "%lf", &g_epsilon);
   fscanf(f, "%d", &g_max_iterations);
   fscanf(f, "%lf %lf %lf %lf", &g_u_min, &g_u_max, &g_v_min, &g_v_max);
   fscanf(f, "%d %d", &g_width, &g_height);
+  fscanf(f, "%lf %lf", &g_complex_const_re, &g_complex_const_im);
 
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
@@ -65,6 +87,7 @@ int main(int argc, char **argv) {
 
   glutCreateWindow("Objects");
   glutDisplayFunc(display);
+  glutKeyboardFunc(keyboard);
   glutMainLoop();
 
   return 0;
